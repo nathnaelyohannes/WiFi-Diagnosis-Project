e04bc0efc7677b43ec05eddf761e9806be3b8e52
Nathnael Yohannes <115045930+nathnaelyohannes@users.noreply.github.com>
2023-12-04 11:27:10 -0500

    Update WiFiDiagnosis.java

--- WiFiDiagnosis.java
+++ WiFiDiagnosis.java
@@ -1,16 +1,3 @@
-# CS203
-/*
- * Class: CMSC203 31569
- * Instructor: Ahmed Tarek
- * Due: 02/07/2023 
- * Platform/compiler:Eclipse
- * I pledge that I have completed the programming assignment independently.
-   I have not copied the code from a student or any source.
-   I have not given my code to any student.
-   Print your Name here: Nathnael Yohannes
-*/
-
-
 
 import java.util.Scanner;
 public class WiFiDiagnosis {
